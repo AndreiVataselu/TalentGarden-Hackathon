fe67320c1e83ae6e75f88edd2a4b9c44051ec201
Andrei Vataselu <>
2019-11-23 00:47:00 +0100

    Initial commit

---
+++ TalentGarden Hackathon/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TalentGarden Hackathon
+//
+//  Created by Andrei Vătășelu on 23/11/2019.
+//  Copyright © 2019 Andrei Vătășelu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ TalentGarden Hackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TalentGarden Hackathon
+//
+//  Created by Andrei Vătășelu on 23/11/2019.
+//  Copyright © 2019 Andrei Vătășelu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
